7cb3e9d25a30a6f22b821974fbbdd80c50d94826
Aida Jakli <aidajakli@gmail.com>
2021-02-24 20:42:59 +0100

    user service added

---
+++ PmBackend.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using PmBackend.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PmBackend.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        User GetUser(int userId);
+        IEnumerable<User> GetUsers();
+        User InsertUser(User newUser);
+        void UpdateUser(int userId, User UpdatedUser);
+        void DeleteUser(int userId);
+    }
+}
---
+++ PmBackend.BLL/Services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PmBackend.BLL.Interfaces;
+using PmBackend.DAL;
+using PmBackend.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PmBackend.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly PmDbContext _ctx;
+        public UserService(PmDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public void DeleteUser(int userId)
+        {
+            _ctx.Users.Remove(new User { Id = userId });
+            _ctx.SaveChanges();
+        }
+
+        public User GetUser(int userId)
+        {
+            return _ctx.Users.SingleOrDefault(u => u.Id == userId);
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return _ctx.Users.ToList();
+        }
+
+        public User InsertUser(User newUser)
+        {
+            _ctx.Users.Add(newUser);
+            _ctx.SaveChanges();
+            return newUser;
+        }
+
+        public void UpdateUser(int userId, User updatedUser)
+        {
+            updatedUser.Id = userId;
+            var u = _ctx.Attach(updatedUser);
+            u.State = EntityState.Modified;
+            _ctx.SaveChanges();
+        }
+    }
+}
